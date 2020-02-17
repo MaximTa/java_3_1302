ccb4b51d1be07db04b84d297c26696d194d6e637
Maxim Tarasyants <macartoor@gmail.com>
2020-02-17 11:20:42 +0300

    Initial commit

---
+++ src/ru/geekbrains/java/Main.java
@@ -0,0 +1,32 @@
+package ru.geekbrains.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Угадай число с трёх раз!");
+        int range = 9;
+        int number = (int) (Math.random() * range);
+        playLevel(number);
+        scanner.close();
+    }
+    private static void playLevel(int number) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Угадай число от 0 до 9");
+            int input_number = scanner.nextInt();
+            if (input_number == number) {
+                System.out.println("Вы угадали.");
+                break;
+            } else if (i == 2) {
+                System.out.println("Количество попыток закончилось. Вы не угадали.");
+            } else if (input_number > number) {
+                System.out.println("Число меньше.");
+            } else {
+                System.out.println("Число больше.");
+            }
+        }
+    }
+}
